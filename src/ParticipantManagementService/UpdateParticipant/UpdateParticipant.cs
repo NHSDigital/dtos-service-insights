d3af2b9817e742f2d01803e08cc06aa97161c4a3
OswellHita <oswell.hita1@nhs.net>
2024-07-25 16:25:06 +0100

    refactor: Moved Participant class to separate file

--- src/ParticipantManagementService/UpdateParticipant/UpdateParticipant.cs
+++ src/ParticipantManagementService/UpdateParticipant/UpdateParticipant.cs
@@ -12,43 +12,35 @@ public class UpdateParticipant
 {
     private readonly ILogger<UpdateParticipant> _logger;
 
-
     public UpdateParticipant(ILogger<UpdateParticipant> logger)
     {
-        _logger = logger;
-
+      _logger = logger;
     }
 
     [Function("updateParticipant")]
     public  HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
-        Participant  participant;
-try
-{
-      using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
-              {
-                  var postData = reader.ReadToEnd();
-                  participant = JsonSerializer.Deserialize<Participant>(postData);
-              }
+      Participant  participant;
+      try
+      {
+        using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
+        {
+            var postData = reader.ReadToEnd();
+            participant = JsonSerializer.Deserialize<Participant>(postData);
+        }
 
-              _logger.LogInformation(participant.NhsNumber);
+        _logger.LogInformation(participant.NhsNumber);
 
-              return req.CreateResponse(HttpStatusCode.OK);
+        return req.CreateResponse(HttpStatusCode.OK);
 
-}
-catch
-{
-      _logger.LogError("Could not read participant");
+      }
+      catch
+      {
+        _logger.LogError("Could not read participant");
 
-      return req.CreateResponse(HttpStatusCode.BadRequest);
-}
-
+        return req.CreateResponse(HttpStatusCode.BadRequest);
+      }
     }
-
-    }
-
-public class Participant
-  {
-    public string NhsNumber { get; set; }
   }
+
 
