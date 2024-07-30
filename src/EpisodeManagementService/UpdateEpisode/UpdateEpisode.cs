86f48d57a3206e0288d2ec98f1919d74ebff6049
OswellHita <oswell.hita1@nhs.net>
2024-07-30 16:00:45 +0100

    created update-episode-function

---
+++ src/EpisodeManagementService/UpdateEpisode/UpdateEpisode.cs
@@ -0,0 +1,46 @@
+namespace updateParticipant;
+
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json;
+
+
+public class UpdateEpisode
+{
+    private readonly ILogger<UpdateEpisode> _logger;
+
+    public UpdateEpisode(ILogger<UpdateEpisode> logger)
+    {
+      _logger = logger;
+    }
+
+    [Function("updateEpisode")]
+    public  HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+      Episode  episode;
+      try
+      {
+        using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
+        {
+            var postData = reader.ReadToEnd();
+            episode = JsonSerializer.Deserialize<Episode>(postData);
+        }
+
+        _logger.LogInformation(episode.episode_id);
+
+        return req.CreateResponse(HttpStatusCode.OK);
+
+      }
+      catch
+      {
+        _logger.LogError("Could not read episode");
+
+        return req.CreateResponse(HttpStatusCode.BadRequest);
+      }
+    }
+  }
+
+
