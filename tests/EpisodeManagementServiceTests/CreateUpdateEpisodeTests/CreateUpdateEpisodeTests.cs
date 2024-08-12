5e15be0c08a45020d0c8ad8210319b2e7fc65928
Alex Clayton <168740950+alex-clayton-1@users.noreply.github.com>
2024-08-12 13:56:15 +0100

    feat: Implemented CreateEpisode function in EpisodeDataService (#17)
    * feat: Implemented Create Episode function in Episode Data Service
    
    * test: Fixed tests
    
    * style: Fixed file format issues
    
    * refactor: Now using HttpClient for http requests in Common
    
    * refactor: Updated namespaces
    
    * build: Updated solution file
    
    * refactor: Moved EF generated models in Model class library
    
    * refactor: Renamed Common namespace
    
    * style: Fixed file format issues
    
    * refactor: Refactored SendHttpRequestAsync method
    
    * style: Moved namespaces below using directives
    
    * refactor: Removed old Episode model class
    
    * fix: Fixed build errors

---
+++ tests/EpisodeManagementServiceTests/CreateUpdateEpisodeTests/CreateUpdateEpisodeTests.cs
@@ -0,0 +1,95 @@
+namespace NHS.ServiceInsights.EpisodeManagementServiceTests;
+
+using Moq;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker.Http;
+using NHS.ServiceInsights.EpisodeManagementService;
+using NHS.ServiceInsights.TestUtils;
+using NHS.ServiceInsights.Model;
+using NHS.ServiceInsights.Common;
+
+[TestClass]
+public class CreateUpdateEpisodeTests
+{
+    private readonly Mock<ILogger<CreateUpdateEpisode>> _logger = new();
+    private readonly Mock<IHttpRequestService> _httpRequestService = new();
+    private Mock<HttpRequestData> _request;
+    private readonly SetupRequest _setupRequest = new();
+    private readonly CreateUpdateEpisode _sut;
+
+    public CreateUpdateEpisodeTests()
+    {
+        Environment.SetEnvironmentVariable("CreateEpisodeUrl", "CreateEpisodeUrl");
+
+        _sut = new CreateUpdateEpisode(_logger.Object, _httpRequestService.Object);
+    }
+
+    [TestMethod]
+    public async Task Run_Return_OK_When_Create_Episode_Succeeds()
+    {
+        // Arrange
+        var episode = new Episode
+        {
+            EpisodeId = "1234567890"
+        };
+
+        var json = JsonSerializer.Serialize(episode);
+        _request = _setupRequest.Setup(json);
+
+        // Act
+        var result = await _sut.Run(_request.Object);
+
+        // Assert
+        _httpRequestService.Verify(x => x.SendPost("CreateEpisodeUrl", It.IsAny<string>()), Times.Once);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task Run_Return_InternalServerError_When_Create_Episode_Fails()
+    {
+        // Arrange
+        var episode = new Episode
+        {
+            EpisodeId = "1234567890"
+        };
+
+        var json = JsonSerializer.Serialize(episode);
+        _request = _setupRequest.Setup(json);
+
+        _httpRequestService.Setup(x => x.SendPost("CreateEpisodeUrl", It.IsAny<string>())).Throws<Exception>();
+
+        // Act
+        var result = await _sut.Run(_request.Object);
+
+        // Assert
+        _httpRequestService.Verify(x => x.SendPost("CreateEpisodeUrl", It.IsAny<string>()), Times.Once);
+        Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task Run_Should_Return_BadRequest_When_Episode_Is_Invalid()
+    {
+        // Arrange
+        var json = JsonSerializer.Serialize("Invalid");
+        _request = _setupRequest.Setup(json);
+
+        // Act
+        var result = await _sut.Run(_request.Object);
+
+        // Assert
+        _logger.Verify(log =>
+            log.Log(
+            LogLevel.Error,
+            0,
+            It.Is<It.IsAnyType>((state, type) => state.ToString() == "Could not read episode data."),
+            null,
+            (Func<object, Exception, string>)It.IsAny<object>()),
+            Times.Once);
+        _httpRequestService.Verify(x => x.SendPost("CreateEpisodeUrl", It.IsAny<string>()), Times.Never);
+        Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+    }
+}
