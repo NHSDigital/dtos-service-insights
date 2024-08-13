0500626161c18c1e97296b60196fbf778f5482bc
OswellHita <oswell.hita1@nhs.net>
2024-08-13 11:25:33 +0100

    refactor: Updated GetParticipant Azure function code to handle GET requests.

--- src/ParticipantManagementService/GetParticipant/GetParticipant.cs
+++ src/ParticipantManagementService/GetParticipant/GetParticipant.cs
@@ -1,10 +1,8 @@
-using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System.Threading.Tasks;
 
 namespace GetParticipant
@@ -13,29 +11,26 @@ namespace GetParticipant
     {
         [FunctionName("GetParticipant")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("Request to retrieve a participant has been processed.");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var data = JsonConvert.DeserializeObject<dynamic>(requestBody);
-            string NhsNumber = data?.nhs_number;
+            string NhsNumber = req.Query["nhs_number"];
 
             if (string.IsNullOrEmpty(NhsNumber))
             {
                 return new BadRequestObjectResult("Please enter a valid NHS Number.");
             }
 
-            var participant = ParticipantRepository.GetParticipantByNhsNumber(NhsNumber);
+            var participant = await Task.Run(() => ParticipantRepository.GetParticipantByNhsNumber(NhsNumber));
 
             if (participant == null)
             {
-                return new NotFoundObjectResult($"Participant with ID {NhsNumber} not found.");
+                return new NotFoundObjectResult($"Participant with NHS Number {NhsNumber} not found.");
             }
 
             return new OkObjectResult(participant);
         }
-
     }
 }
