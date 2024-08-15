76e1a881c97372bc2684e64708a20ebe86680e79
OswellHita <oswell.hita1@nhs.net>
2024-08-15 10:40:50 +0100

    refactor: changed function runtime to dotnet-isolated and refactored code

--- src/ParticipantManagementService/GetParticipant/GetParticipant.cs
+++ src/ParticipantManagementService/GetParticipant/GetParticipant.cs
@@ -1,36 +1,48 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
+using System.Text.Json;
 
-namespace GetParticipant
+
+namespace NHS.ServiceInsights.ParticipantManagementService;
+
+public class GetParticipant
 {
-    public static class GetParticipant
+    private readonly ILogger<GetParticipant> _logger;
+
+
+public GetParticipant(ILogger<GetParticipant> logger)
+{
+    _logger = logger;
+
+}
+
+    [Function("GetParticipant")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
     {
-        [FunctionName("GetParticipant")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+        _logger.LogInformation("Request to retrieve a participant has been processed.");
+
+        string NhsNumber = req.Query["nhs_number"];
+
+        if (string.IsNullOrEmpty(NhsNumber))
         {
-            log.LogInformation("Request to retrieve a participant has been processed.");
-
-            string NhsNumber = req.Query["nhs_number"];
-
-            if (string.IsNullOrEmpty(NhsNumber))
-            {
-                return new BadRequestObjectResult("Please enter a valid NHS Number.");
-            }
-
-            var participant = await Task.Run(() => ParticipantRepository.GetParticipantByNhsNumber(NhsNumber));
-
-            if (participant == null)
-            {
-                return new NotFoundObjectResult($"Participant with NHS Number {NhsNumber} not found.");
-            }
-
-            return new OkObjectResult(participant);
+            _logger.LogError("Please enter a valid NHS Number.");
+            return req.CreateResponse(HttpStatusCode.BadRequest);
         }
+
+        var participant = ParticipantRepository.GetParticipantByNhsNumber(NhsNumber);
+
+        if (participant == null)
+        {
+            _logger.LogError($"Participant with NHS Number {NhsNumber} not found.");
+            return req.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "application/json");
+        var json = JsonSerializer.Serialize(participant);
+        await response.WriteStringAsync(json);
+        return response;
     }
 }
