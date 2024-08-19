37081b9d2cbb90c32dfb7af5c4d202e1ed9136fe
millsotron-ibm <78595204+millsotron-ibm@users.noreply.github.com>
2024-08-19 12:11:09 +0100

    feat: Implemented GetParticipant function in Participant Management Service (#16)
    * feat/Add GetParticipant function
    
    * chore/prettier file format
    
    * feat/set port for GetParticipant function
    
    * refactor: changed the log messages and removed  comments.
    
    * refactor: updated code to streamline the get participant logic
    
    * refactor: updated code to streamline the get participant logic
    
    * chore: ran prettier to format Participant.cs file
    
    * refactor: Updated GetParticipant Azure function code to handle GET requests.
    
    * refactor: Moved Participant Class file to shared model folder
    
    * refactor: changed function runtime to dotnet-isolated and refactored code
    
    * feat: Created test script for GetParticipant function
    
    * refactor: Updated GetParticipantsTests cs and project files
    
    * chore: removed extensions.json file
    
    * chore: renamed the local.settings.json file
    
    * chore: deleted local.settings.json file
    
    * refactor: Updated the GetParticipantsTests code.
    
    ---------
    
    Co-authored-by: OswellHita <oswell.hita1@nhs.net>

---
+++ src/ParticipantManagementService/GetParticipant/GetParticipant.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+
+namespace NHS.ServiceInsights.ParticipantManagementService;
+
+public class GetParticipant
+{
+    private readonly ILogger<GetParticipant> _logger;
+
+
+public GetParticipant(ILogger<GetParticipant> logger)
+{
+    _logger = logger;
+
+}
+
+    [Function("GetParticipant")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+    {
+        _logger.LogInformation("Request to retrieve a participant has been processed.");
+
+        string NhsNumber = req.Query["nhs_number"];
+
+        if (string.IsNullOrEmpty(NhsNumber))
+        {
+            _logger.LogError("Please enter a valid NHS Number.");
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        var participant = ParticipantRepository.GetParticipantByNhsNumber(NhsNumber);
+
+        if (participant == null)
+        {
+            _logger.LogError($"Participant with NHS Number {NhsNumber} not found.");
+            return req.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "application/json");
+        var json = JsonSerializer.Serialize(participant);
+        await response.WriteStringAsync(json);
+        return response;
+    }
+}
