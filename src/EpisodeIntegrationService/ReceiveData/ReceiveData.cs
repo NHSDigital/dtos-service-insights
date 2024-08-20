ab7edf5c31a18f5f315abaafe0328f7d25baaf1d
millsotron-ibm <78595204+millsotron-ibm@users.noreply.github.com>
2024-08-20 11:11:05 +0100

    feat: send participant and episode data from episode integration service to participant and episode management services (#15)
    * feat/Updated functions to split data for downstream processing
    
    * feat/add test data cases
    
    * chore/prettier format happy path test data
    
    * update Episode and Participant unit tests
    
    * chore/amend mock data
    
    * feat/add unit tests for Receive and Process data
    
    * refactor/partial ProcessData to use IHttpRequestService
    
    * refactor/ProcessData WIP
    
    * fix: Fixed some build errors
    
    * feat/vs code config
    
    * chore/bump dotnet 6 to 8
    
    * refactor/ProcessData function
    
    * chore/ProcessData add logging
    
    * refactor/ProcessData and ReceiveData
    
    * refactor/Participant ok, Episode null
    
    * refactor/additional logging in Episode functions
    
    * refactor/working episode and participant downstream
    
    * chore/prettier
    
    * refactor/tidy up
    CreateUpdateEpisode
    
    * feat/update gitignore
    
    * refactor/reduce logging
    
    * refactor/revert UpdateParticipant code to main version
    
    * refactor/use file based namespace
    
    * refactor/dotnet isolated
    
    * refactor/remove .vscode folders unused
    
    * refactor/reverted missing Participant code
    
    * refactor/receive data function to use HttpRequestService
    
    * refactor:working processdata function serialisation
    
    * test: add ProcessData tests
    
    * style: prettier
    
    * chore: remove unused
    
    * chore: remove unused logs
    
    ---------
    
    Co-authored-by: OswellHita <oswell.hita1@nhs.net>
    Co-authored-by: alex-clayton-1 <alexander.clayton1@nhs.net>

--- src/EpisodeIntegrationService/ReceiveData/ReceiveData.cs
+++ src/EpisodeIntegrationService/ReceiveData/ReceiveData.cs
@@ -1,70 +1,56 @@
-using System.IO;
-using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Collections.Generic;
+using Microsoft.Azure.Functions.Worker;
+using System.Text.Json;
+using NHS.ServiceInsights.Common;
 
-public static class BlobJsonTrigger
+namespace NHS.ServiceInsights.EpisodeManagementService;
+
+public class ReceiveData
 {
-  [FunctionName("receiveData")]
-  public static async Task Run(
-      [BlobTrigger("sample-container/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob,
-      string name,
-      ILogger log)
-  {
-    log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
-    // Validate JSON
-    using (var reader = new StreamReader(myBlob))
-    {
-      var jsonData = reader.ReadToEnd();
-      if (IsValidJson(jsonData))
-      {
-        log.LogInformation("JSON is valid.");
-        await SendToProcessDataFunction(jsonData, log);
-      }
-      else
-      {
-        log.LogError("JSON is invalid.");
-      }
-    }
-  }
+    private readonly ILogger<ReceiveData> _logger;
+    private readonly IHttpRequestService _httpRequestService;
 
-  private static bool IsValidJson(string jsonData)
-  {
-    try
+    public ReceiveData(ILogger<ReceiveData> logger, IHttpRequestService httpRequestService)
     {
-      var obj = JsonConvert.DeserializeObject<object>(jsonData);
-      return obj != null;
+        _logger = logger;
+        _httpRequestService = httpRequestService;
     }
-    catch (JsonException)
+    [Function("ReceiveData")]
+    public async Task Run(
+        [BlobTrigger("sample-container/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob,
+        string name)
     {
-      return false;
-    }
-  }
+        _logger.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
-  private static async Task SendToProcessDataFunction(string jsonData, ILogger log)
-  {
-    var functionUrl = "http://localhost:7111/api/ProcessData";
-    using (var client = new HttpClient())
-    {
-      var content = new StringContent(JsonConvert.SerializeObject(new { Data = jsonData }));
-      content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-      var response = await client.PostAsync(functionUrl, content);
-      if (response.IsSuccessStatusCode)
-      {
-        log.LogInformation("Data sent to ProcessData function successfully.");
-      }
-      else
-      {
-        log.LogError("Failed to send data to ProcessData function.");
-      }
+        // Validate JSON
+        using (var reader = new StreamReader(myBlob))
+        {
+            var jsonData = reader.ReadToEnd();
+            if (IsValidJson(jsonData))
+            {
+                _logger.LogInformation("JSON is valid.");
+                await _httpRequestService.SendPost(Environment.GetEnvironmentVariable("ProcessDataURL"), jsonData);
+            }
+            else
+            {
+                _logger.LogError("JSON is invalid.");
+            }
+        }
     }
-  }
+
+    private bool IsValidJson(string jsonData)
+    {
+        try
+        {
+            _logger.LogInformation("JSON is valid:{jsonData}", jsonData);
+            var obj = JsonSerializer.Deserialize<object>(jsonData);
+            return obj != null;
+        }
+        catch (JsonException)
+        {
+            _logger.LogError("Could not validate JSON");
+            return false;
+        }
+    }
 }
--- src/ParticipantManagementService/UpdateParticipant/UpdateParticipant.cs
+++ src/ParticipantManagementService/UpdateParticipant/UpdateParticipant.cs
@@ -6,41 +6,45 @@ using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using System.Text;
 using System.Text.Json;
+using NHS.ServiceInsights.Common;
+using NHS.ServiceInsights.Model;
 
 
 public class UpdateParticipant
 {
     private readonly ILogger<UpdateParticipant> _logger;
+    private readonly IHttpRequestService _httpRequestService;
 
-    public UpdateParticipant(ILogger<UpdateParticipant> logger)
+    public UpdateParticipant(ILogger<UpdateParticipant> logger, IHttpRequestService httpRequestService)
     {
-      _logger = logger;
+        _logger = logger;
+        _httpRequestService = httpRequestService;
     }
 
     [Function("updateParticipant")]
-    public  HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+
     {
-      Participant  participant;
-      try
-      {
-        using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
+        Participant participant;
+        try
         {
-            var postData = reader.ReadToEnd();
-            participant = JsonSerializer.Deserialize<Participant>(postData);
+            using (StreamReader reader = new StreamReader(req.Body, Encoding.UTF8))
+            {
+                var postData = reader.ReadToEnd();
+                participant = JsonSerializer.Deserialize<Participant>(postData);
+                _logger.LogInformation("PostData: {postData}", postData);
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
+
         }
+        catch
+        {
+            _logger.LogError("Could not read participant");
 
-        _logger.LogInformation(participant.NhsNumber);
-
-        return req.CreateResponse(HttpStatusCode.OK);
-
-      }
-      catch
-      {
-        _logger.LogError("Could not read participant");
-
-        return req.CreateResponse(HttpStatusCode.BadRequest);
-      }
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
     }
-  }
+}
 
 
