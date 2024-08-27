dc2fe3bc4b466b5dad69e4fed20e15fb6a9dda4f
millsotron-ibm <christopher.mills-cic-uk@ibm.com>
2024-08-27 15:41:32 +0100

    feature: update episode table creation script to match model

--- scripts/database/create_tables.sql
+++ scripts/database/create_tables.sql
@@ -9,28 +9,21 @@ IF NOT EXISTS
     AND TABLE_NAME = 'EPISODE'
 )
 BEGIN
-    -- CREATE TABLE EPISODE
-    -- (
-    --     EPISODE_ID BIGINT NOT NULL,
-    --     CONSTRAINT PK_EPISODE
-    --         PRIMARY KEY (EPISODE_ID)
-    -- );
     CREATE TABLE EPISODE
     (
-         EPISODE_ID BIGINT NOT NULL,
+      EPISODE_ID BIGINT NOT NULL,
         CONSTRAINT PK_EPISODE
-            PRIMARY KEY (EPISODE_ID),
-    [EPISODE_TYPE]                     NVARCHAR (50) NULL,
-    [BSO_ORGANISATION_CODE]            NVARCHAR (50) NULL,
-    [BSO_BATCH_ID]                     NVARCHAR (50) NULL,
-    [EPISODE_DATE]                     NVARCHAR (50) NULL,
-    [END_CODE]                         NVARCHAR (50) NULL,
-    [DATE_OF_FOA]                      NVARCHAR (50) NULL,
-    [DATE_OF_AS]                       NVARCHAR (50) NULL,
-    [APPOINTMENT_MADE]                 NVARCHAR (50) NULL,
-    [CALL_RECALL_STATUS_AUTHORISED_BY] NVARCHAR (50) NULL,
-    [EARLY_RECALL_DATE]                NVARCHAR (50) NULL,
-    [END_CODE_LAST_UPDATED]            NVARCHAR (50) NULL,
-    CONSTRAINT [PK_EPISODE] PRIMARY KEY CLUSTERED ([EPISODE_ID] ASC)
+          PRIMARY KEY (EPISODE_ID),
+      EPISODE_TYPE                     NVARCHAR (50) NULL,
+      BSO_ORGANISATION_CODE            NVARCHAR (50) NULL,
+      BSO_BATCH_ID                     NVARCHAR (50) NULL,
+      EPISODE_DATE                     NVARCHAR (50) NULL,
+      END_CODE                         NVARCHAR (50) NULL,
+      DATE_OF_FOA                      NVARCHAR (50) NULL,
+      DATE_OF_AS                       NVARCHAR (50) NULL,
+      APPOINTMENT_MADE                 NVARCHAR (50) NULL,
+      CALL_RECALL_STATUS_AUTHORISED_BY NVARCHAR (50) NULL,
+      EARLY_RECALL_DATE                NVARCHAR (50) NULL,
+      END_CODE_LAST_UPDATED            NVARCHAR (50) NULL
 );
 END
