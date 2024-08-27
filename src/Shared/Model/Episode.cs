4140ea301e581d3abdf2740241ae6ca7ff0524e1
millsotron-ibm <christopher.mills-cic-uk@ibm.com>
2024-08-27 15:36:25 +0100

    feature: additional Episode columns to data model

--- src/Shared/Model/Episode.cs
+++ src/Shared/Model/Episode.cs
@@ -6,4 +6,16 @@ namespace NHS.ServiceInsights.Model;
 public partial class Episode
 {
     public long EpisodeId { get; set; }
+    public string? Episode_Type { get; set; }
+    public string? Bso_Organisation_Code { get; set; }
+    public string? Episode_Date { get; set; }
+    public string? End_Code { get; set; }
+    public string? Date_Of_Foa { get; set; }
+    public string? Date_Of_As { get; set; }
+    public string? Appointment_Made { get; set; }
+    public string? Call_Recall_Status_Authorised_By { get; set; }
+    public string? Early_Recall_Date { get; set; }
+    public string? End_Code_Last_Updated { get; set; }
+    public string? Bso_Batch_Id { get; set; }
+
 }
