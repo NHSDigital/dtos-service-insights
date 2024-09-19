2d1abf7ca7de4108cc29c8e634cf53f2da9f2e27
millsotron-ibm <78595204+millsotron-ibm@users.noreply.github.com>
2024-09-19 14:43:12 +0100

    feat: add participant screening profile participant screening episode entity models (#27)
    * feat: add participant tables
    
    * feat: add entity framework scaffold created models
    
    * chore: prettier
    
    * chore: prettier
    
    * chore: fix typo
    
    * chore: entity framework re-scaffold
    
    * chore: prettier DBContext
    
    * chore: revert unwanted change to episode model
    
    * chore: revert unwanted model changes
    
    * chore: prettier
    
    * chore: remove unused dotnet-mesh-client folder

---
+++ src/Shared/Model/ParticipantScreeningEpisode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NHS.ServiceInsights.Model;
+
+public partial class ParticipantScreeningEpisode
+{
+    public string? EpisodeId { get; set; }
+
+    public string? ScreeningName { get; set; }
+
+    public string? NhsNumber { get; set; }
+
+    public string? EpisodeType { get; set; }
+
+    public string? EpisodeTypeDescription { get; set; }
+
+    public string? EpisodeOpenDate { get; set; }
+
+    public string? AppointmentMadeFlag { get; set; }
+
+    public string? FirstOfferedAppointmentDate { get; set; }
+
+    public string? ActualScreeningDate { get; set; }
+
+    public string? EarlyRecallDate { get; set; }
+
+    public string? CallRecallStatusAuthorisedBy { get; set; }
+
+    public string? EndCode { get; set; }
+
+    public string? EndCodeDescription { get; set; }
+
+    public string? EndCodeLastUpdated { get; set; }
+
+    public string? OrganisationCode { get; set; }
+
+    public string? OrganisationName { get; set; }
+
+    public string? BatchId { get; set; }
+
+    public string? RecordInsertDatetime { get; set; }
+}
