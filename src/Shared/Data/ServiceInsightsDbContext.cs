ada1f230a75b2f964f738530af84241b14e3a802
millsotron-ibm <christopher.mills-cic-uk@ibm.com>
2024-09-19 12:16:43 +0100

    chore: revert unwanted model changes

--- src/Shared/Data/ServiceInsightsDbContext.cs
+++ src/Shared/Data/ServiceInsightsDbContext.cs
@@ -129,56 +129,41 @@ public partial class ServiceInsightsDbContext : DbContext
             entity.ToTable("EPISODE");
 
             entity.Property(e => e.EpisodeId)
-                .HasMaxLength(50)
+                .ValueGeneratedNever()
                 .HasColumnName("EPISODE_ID");
-            entity.Property(e => e.ActualScreeningDate)
+            entity.Property(e => e.AppointmentMade)
                 .HasMaxLength(50)
-                .HasColumnName("ACTUAL_SCREENING_DATE");
-            entity.Property(e => e.AppointmentMadeFlag)
+                .HasColumnName("APPOINTMENT_MADE");
+            entity.Property(e => e.BsoBatchId)
                 .HasMaxLength(50)
-                .HasColumnName("APPOINTMENT_MADE_FLAG");
-            entity.Property(e => e.BatchId)
+                .HasColumnName("BSO_BATCH_ID");
+            entity.Property(e => e.BsoOrganisationCode)
                 .HasMaxLength(50)
-                .HasColumnName("BATCH_ID");
+                .HasColumnName("BSO_ORGANISATION_CODE");
             entity.Property(e => e.CallRecallStatusAuthorisedBy)
                 .HasMaxLength(50)
                 .HasColumnName("CALL_RECALL_STATUS_AUTHORISED_BY");
+            entity.Property(e => e.DateOfAs)
+                .HasMaxLength(50)
+                .HasColumnName("DATE_OF_AS");
+            entity.Property(e => e.DateOfFoa)
+                .HasMaxLength(50)
+                .HasColumnName("DATE_OF_FOA");
             entity.Property(e => e.EarlyRecallDate)
                 .HasMaxLength(50)
                 .HasColumnName("EARLY_RECALL_DATE");
-            entity.Property(e => e.EndCodeId)
+            entity.Property(e => e.EndCode)
                 .HasMaxLength(50)
-                .HasColumnName("END_CODE_ID");
+                .HasColumnName("END_CODE");
             entity.Property(e => e.EndCodeLastUpdated)
                 .HasMaxLength(50)
                 .HasColumnName("END_CODE_LAST_UPDATED");
-            entity.Property(e => e.EpisodeOpenDate)
+            entity.Property(e => e.EpisodeDate)
                 .HasMaxLength(50)
-                .HasColumnName("EPISODE_OPEN_DATE");
-            entity.Property(e => e.EpisodeTypeId)
+                .HasColumnName("EPISODE_DATE");
+            entity.Property(e => e.EpisodeType)
                 .HasMaxLength(50)
-                .HasColumnName("EPISODE_TYPE_ID");
-            entity.Property(e => e.FirstOfferedAppointmentDate)
-                .HasMaxLength(50)
-                .HasColumnName("FIRST_OFFERED_APPOINTMENT_DATE");
-            entity.Property(e => e.NhsNumber)
-                .HasMaxLength(50)
-                .HasColumnName("NHS_NUMBER");
-            entity.Property(e => e.OrganisationId)
-                .HasMaxLength(50)
-                .HasColumnName("ORGANISATION_ID");
-            entity.Property(e => e.ParticipantId)
-                .HasMaxLength(50)
-                .HasColumnName("PARTICIPANT_ID");
-            entity.Property(e => e.RecordInsertDatetime)
-                .HasMaxLength(50)
-                .HasColumnName("RECORD_INSERT_DATETIME");
-            entity.Property(e => e.RecordUpdateDatetime)
-                .HasMaxLength(50)
-                .HasColumnName("RECORD_UPDATE_DATETIME");
-            entity.Property(e => e.ScreeningId)
-                .HasMaxLength(50)
-                .HasColumnName("SCREENING_ID");
+                .HasColumnName("EPISODE_TYPE");
         });
 
         modelBuilder.Entity<ParticipantScreeningEpisode>(entity =>
