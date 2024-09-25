11f8017cb846d30a4a11b05a3075ecf76c8f7741
OswellHita <oswell.hita1@nhs.net>
2024-09-25 11:47:42 +0100

    test: Implement episode existence checks and update logic in CreateUpdateEpisode

--- tests/EpisodeManagementServiceTests/CreateUpdateEpisodeTests/CreateUpdateEpisodeTests.cs
+++ tests/EpisodeManagementServiceTests/CreateUpdateEpisodeTests/CreateUpdateEpisodeTests.cs
@@ -22,32 +22,60 @@ public class CreateUpdateEpisodeTests
     public CreateUpdateEpisodeTests()
     {
         Environment.SetEnvironmentVariable("CreateEpisodeUrl", "CreateEpisodeUrl");
+        Environment.SetEnvironmentVariable("UpdateEpisodeUrl", "UpdateEpisodeUrl");
+        Environment.SetEnvironmentVariable("GetEpisodeUrl", "GetEpisodeUrl");
 
         _function = new CreateUpdateEpisode(_mockLogger.Object, _mockHttpRequestService.Object);
     }
 
     [TestMethod]
-    public async Task Run_Return_OK_When_Create_Episode_Succeeds()
+    public async Task Run_Returns_OK_And_Creates_Episode_When_Episode_Does_Not_Exist()
     {
         // Arrange
         var episode = new Episode
         {
-            EpisodeId = "1234567890"
+            EpisodeId = "123456"
         };
 
         var json = JsonSerializer.Serialize(episode);
         _mockRequest = _setupRequest.Setup(json);
 
+        _mockHttpRequestService.Setup(x => x.SendGet(It.IsAny<string>())).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.NotFound));
+
         // Act
         var result = await _function.Run(_mockRequest.Object);
 
         // Assert
+        _mockHttpRequestService.Verify(x => x.SendGet(It.IsAny<string>()), Times.Once);
         _mockHttpRequestService.Verify(x => x.SendPost("CreateEpisodeUrl", It.IsAny<string>()), Times.Once);
         Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
     }
 
     [TestMethod]
-    public async Task Run_Return_InternalServerError_When_Create_Episode_Fails()
+    public async Task Run_Return_OK_When_Episode_Already_Exists()
+    {
+        // Arrange
+        var episode = new Episode
+        {
+            EpisodeId = "234567"
+        };
+
+        var json = JsonSerializer.Serialize(episode);
+        _mockRequest = _setupRequest.Setup(json);
+
+        _mockHttpRequestService.Setup(x => x.SendGet(It.IsAny<string>())).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK));
+
+        // Act
+        var result = await _function.Run(_mockRequest.Object);
+
+        // Assert
+        _mockHttpRequestService.Verify(x => x.SendGet(It.IsAny<string>()), Times.Once);
+        _mockHttpRequestService.Verify(x => x.SendPut("UpdateEpisodeUrl", It.IsAny<string>()), Times.Once);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task Run_Return_InternalServerError_When_Checking_Episode_Existence_Fails()
     {
         // Arrange
         var episode = new Episode
@@ -58,18 +86,18 @@ public class CreateUpdateEpisodeTests
         var json = JsonSerializer.Serialize(episode);
         _mockRequest = _setupRequest.Setup(json);
 
-        _mockHttpRequestService.Setup(x => x.SendPost("CreateEpisodeUrl", It.IsAny<string>())).Throws<Exception>();
+        _mockHttpRequestService.Setup(x => x.SendGet(It.IsAny<string>())).Throws<Exception>();
 
         // Act
         var result = await _function.Run(_mockRequest.Object);
 
         // Assert
-        _mockHttpRequestService.Verify(x => x.SendPost("CreateEpisodeUrl", It.IsAny<string>()), Times.Once);
+        _mockHttpRequestService.Verify(x => x.SendGet(It.IsAny<string>()), Times.Once);
         Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
     }
 
     [TestMethod]
-    public async Task Run_Should_Return_BadRequest_When_Episode_Is_Invalid()
+    public async Task Run_Return_BadRequest_When_Episode_Data_Is_Invalid()
     {
         // Arrange
         var json = JsonSerializer.Serialize("Invalid");
@@ -83,11 +111,10 @@ public class CreateUpdateEpisodeTests
             log.Log(
             LogLevel.Error,
             0,
-            It.Is<It.IsAnyType>((state, type) => state.ToString() == "Could not read episode data."),
+            It.Is<It.IsAnyType>((state, type) => state.ToString().StartsWith("Could not read episode data. Error:")),
             null,
             (Func<object, Exception, string>)It.IsAny<object>()),
             Times.Once);
-        _mockHttpRequestService.Verify(x => x.SendPost("CreateEpisodeUrl", It.IsAny<string>()), Times.Never);
         Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
     }
 }
