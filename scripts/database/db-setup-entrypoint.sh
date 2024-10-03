6d731e0d8dbe32f3b632debe3e3c52857c56c0f0
OswellHita <oswell.hita1@nhs.net>
2024-10-03 16:23:33 +0100

    fix: updated db-setup to use localhost

--- scripts/database/db-setup-entrypoint.sh
+++ scripts/database/db-setup-entrypoint.sh
@@ -8,7 +8,7 @@ sleep 10s
 ls -lah
 
 # Set up database
-/opt/mssql-tools/bin/sqlcmd -S sql-edge -U SA -P "${PASSWORD}" -i create_database.sql
-/opt/mssql-tools/bin/sqlcmd -S sql-edge -U SA -P "${PASSWORD}" -d ${DB_NAME} -i create_tables.sql
-/opt/mssql-tools/bin/sqlcmd -S sql-edge -U SA -P "${PASSWORD}" -d ${DB_NAME} -i insert_episode_test_data.sql
+/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U SA -P "${PASSWORD}" -i create_database.sql
+/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U SA -P "${PASSWORD}" -d ${DB_NAME} -i create_tables.sql
+/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U SA -P "${PASSWORD}" -d ${DB_NAME} -i insert_episode_test_data.sql
 
