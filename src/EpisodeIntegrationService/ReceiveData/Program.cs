fdb57f58e589f4105b492b148b940c160b5bcca9
OswellHita <oswell.hita1@nhs.net>
2024-10-23 19:07:09 +0100

    feat: added DbContext configuration for ServiceInsights database

--- src/EpisodeIntegrationService/ReceiveData/Program.cs
+++ src/EpisodeIntegrationService/ReceiveData/Program.cs
@@ -1,12 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NHS.ServiceInsights.Common;
+using NHS.ServiceInsights.Data;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
         services.AddSingleton<IHttpRequestService, HttpRequestService>();
+        services.AddDbContext<ServiceInsightsDbContext>(
+            options => options.UseSqlServer(Environment.GetEnvironmentVariable("ServiceInsightsDbConnectionString")));
     })
     .Build();
 host.Run();
