780f8f08af66d8bf1979345033ea98fc67a233d5
OswellHita <oswell.hita1@nhs.net>
2024-11-01 18:44:27 +0000

    feat: created EpisodeDto class for data lookup

---
+++ src/Shared/Model/Dtos/EpisodeDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NHS.ServiceInsights.Model;
+
+public class EpisodeDto
+{
+    public long EpisodeId { get; set; }
+
+    public long? EpisodeIdSystem { get; set; }
+
+    public long ScreeningId { get; set; }
+
+    public long NhsNumber { get; set; }
+
+    public string? EpisodeType { get; set; }
+
+    public DateOnly? EpisodeOpenDate { get; set; }
+
+    public string? AppointmentMadeFlag { get; set; }
+
+    public DateOnly? FirstOfferedAppointmentDate { get; set; }
+
+    public DateOnly? ActualScreeningDate { get; set; }
+
+    public DateOnly? EarlyRecallDate { get; set; }
+
+    public string? CallRecallStatusAuthorisedBy { get; set; }
+
+    public string? EndCode { get; set; }
+
+    public DateTime? EndCodeLastUpdated { get; set; }
+
+    public string? OrganisationCode { get; set; }
+
+    public string? BatchId { get; set; }
+
+
+
+
+}
