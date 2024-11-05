0b27d8b67b80ca084c299f77943215655d8ce018
OswellHita <oswell.hita1@nhs.net>
2024-11-05 18:26:14 +0000

    feat: add lookup repositories class files

---
+++ src/Shared/Model/ReasonClosedCodeLkp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NHS.ServiceInsights.Model;
+
+public partial class ReasonClosedCodeLkp
+{
+    public long ReasonClosedCodeId { get; set; }
+
+    public string ReasonClosedCode { get; set; } = null!;
+
+    public string? ReasonClosedCodeDescription { get; set; }
+
+    public virtual ICollection<Episode> Episodes { get; set; } = new List<Episode>();
+}
