445048def940073f39005969369c9858a4644dd2
OswellHita <oswell.hita1@nhs.net>
2024-11-08 12:39:42 +0000

    fix: corrected merge conflict error

--- src/Shared/Model/OrganisationLkp.cs
+++ src/Shared/Model/OrganisationLkp.cs
@@ -1,27 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace NHS.ServiceInsights.Model;
-
-public partial class OrganisationLkp
-{
-    public string OrganisationId { get; set; } = null!;
-
-    public string? ScreeningName { get; set; }
-
-    public string? OrganisationCode { get; set; }
-
-    public string? OrganisationName { get; set; }
-
-    public string? OrganisationType { get; set; }
-
-    public string? IsActive { get; set; }
-}
-
-using System;
-using System.Collections.Generic;
-
-namespace NHS.ServiceInsights.Model;
+﻿namespace NHS.ServiceInsights.Model;
 
 public partial class OrganisationLkp
 {
