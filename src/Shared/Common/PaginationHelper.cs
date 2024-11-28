651b5cc61629eb70b94409ac25164a66753a24b8
Christopher Mills <christopher.mills-cic-uk@ibm.com>
2024-11-28 09:48:08 +0000

    bugfix: participant date handling

--- src/Shared/Common/PaginationHelper.cs
+++ src/Shared/Common/PaginationHelper.cs
@@ -40,7 +40,7 @@ public static class PaginationHelper
 
     public static string BuildUrl(string baseUrl, int page, int pageSize, DateTime startDate, DateTime endDate)
     {
-        var url = $"{baseUrl}?page={page}&pageSize={pageSize}&startDate={startDate.ToString("dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture)}&endDate={endDate.ToString("dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture)}";
+        var url = $"{baseUrl}?page={page}&pageSize={pageSize}&startDate={startDate.ToString(CultureInfo.InvariantCulture)}&endDate={endDate.ToString(CultureInfo.InvariantCulture)}";
         return url;
     }
 }
