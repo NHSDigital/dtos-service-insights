b2bb7185a60a34698c3c0ab72039e2e14ce28938
pengfei-nhs <pengfei.xue1@nhs.net>
2024-12-02 16:28:12 +0000

    chore: update the file formating

--- tests/EpisodeIntegrationServiceTests/ReceiveDataTests/ReceiveDataTests.cs
+++ tests/EpisodeIntegrationServiceTests/ReceiveDataTests/ReceiveDataTests.cs
@@ -78,11 +78,11 @@ public class ReceiveDataTests
 
 /* Will be re-visted, after date logic will be merged.
 
- [TestMethod]
+    [TestMethod]
     public async Task ReceiveData_LogWarningSendbeforeSendingParticipantDataToDownstreamFunctions()
     {
 
-         // Arrange
+        // Arrange
         string data = "change_db_date_time,nhs_number,superseded_nhs_number,gp_practice_code,bso_organisation_code,next_test_due_date,subject_status_code,early_recall_date,latest_invitation_date,removal_reason,removal_date,reason_for_ceasing_code,is_higher_risk,higher_risk_next_test_due_date,hr_recall_due_date,higher_risk_referral_reason_code,date_irradiated,is_higher_risk_active,gene_code,ntdd_calculation_method,preferred_language\n" +
                 "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,2020-01-11,NORMAL,,2017-99-99,,,,False,,,,,,,,\n" +
                 "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,2019-09-05,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
