2aee2d574bffe4973dca0984bbb5f2598ac4e1de
Christopher Mills <christopher.mills-cic-uk@ibm.com>
2024-12-03 10:09:40 +0000

    docs: update unit test arrange to use YYY-MM-DD

--- tests/EpisodeIntegrationServiceTests/ReceiveDataTests/ReceiveDataTests.cs
+++ tests/EpisodeIntegrationServiceTests/ReceiveDataTests/ReceiveDataTests.cs
@@ -25,12 +25,12 @@ public class ReceiveDataTests
     {
         // Arrange
         string data = "nhs_number,episode_id,episode_type,change_db_date_time,episode_date,appointment_made,date_of_foa,date_of_as,early_recall_date,call_recall_status_authorised_by,end_code,end_code_last_updated,bso_organisation_code,bso_batch_id,reason_closed_code,end_point,final_action_code\n" +
-                    "9000007053,571645,R,2020-03-31 12:11:47.339148+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000009808,333330,R,2020-03-31 12:49:47.513821+01,05/09/2016,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
-                    "9000006316,570294,R,2020-03-31 12:52:13.463901+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000007997,569965,R,2020-03-31 13:06:30.814448+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000007702,574222,R,2020-03-31 13:10:21.420187+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000014174,568703,C,2020-03-31 13:21:37.94545+01,30/12/2016,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV172471J,,,";
+                    "9000007053,571645,R,2020-03-31 12:11:47.339148+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000009808,333330,R,2020-03-31 12:49:47.513821+01,2016-09-05,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
+                    "9000006316,570294,R,2020-03-31 12:52:13.463901+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007997,569965,R,2020-03-31 13:06:30.814448+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007702,574222,R,2020-03-31 13:10:21.420187+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000014174,568703,C,2020-03-31 13:21:37.94545+01,2016-12-30,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV172471J,,,";
 
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
@@ -67,17 +67,39 @@ public class ReceiveDataTests
 
     }
 
+    public async Task ReceiveData_ShouldValidateEpisodeDate_DD_MM_YYYY_Format()
+    {
+        // Arrange
+        string data = "nhs_number,episode_id,episode_type,change_db_date_time,episode_date,appointment_made,date_of_foa,date_of_as,early_recall_date,call_recall_status_authorised_by,end_code,end_code_last_updated,bso_organisation_code,bso_batch_id,reason_closed_code,end_point,final_action_code\n" +
+                    "9000007053,571645,R,31-03-2020 12:11:47.339148+01,11-01-2017,True,,,,SCREENING_OFFICE,SC,31-03-2020 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000009808,333330,R,31-03-2020 12:49:47.513821+01,05-09-2016,True,,,,SCREENING_OFFICE,SC,31-03-2020 00:00:00+01,LAV,LAV000001A,,,\n" +
+                    "9000006316,570294,R,31-03-2020 12:52:13.463901+01,11-01-2017,True,,,,SCREENING_OFFICE,SC,31-03-2020 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007997,569965,R,31-03-2020 13:06:30.814448+01,11-01-2017,True,,,,SCREENING_OFFICE,SC,31-03-2020 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007702,574222,R,31-03-2020 13:10:21.420187+01,11-01-2017,True,,,,SCREENING_OFFICE,SC,31-03-2020 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000014174,568703,C,31-03-2020 13:21:37.94545+01,30-12-2016,True,,,,SCREENING_OFFICE,SC,31-03-2020 00:00:00+01,LAV,LAV172471J,,,";
+
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
+
+        // Act
+        await _function.Run(stream, "bss_episodes_test_data_20240930");
+
+        // Assert
+        _mockHttpRequestService.Verify(x => x.SendPost("EpisodeManagementUrl", It.IsAny<string>()), Times.Exactly(6));
+        _mockHttpRequestService.Verify(x => x.SendPost("ParticipantManagementUrl", It.IsAny<string>()), Times.Exactly(0));
+
+    }
+
     [TestMethod]
     public async Task ReceiveData_ShouldValidateEpisodeDateWhenDelimitedByDash()
     {
         // Arrange
         string data = "nhs_number,episode_id,episode_type,change_db_date_time,episode_date,appointment_made,date_of_foa,date_of_as,early_recall_date,call_recall_status_authorised_by,end_code,end_code_last_updated,bso_organisation_code,bso_batch_id,reason_closed_code,end_point,final_action_code\n" +
-                    "9000007053,571645,R,2020-03-31 12:11:47.339148+01,11-01-2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000009808,333330,R,2020-03-31 12:49:47.513821+01,05-09-2016,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
-                    "9000006316,570294,R,2020-03-31 12:52:13.463901+01,11-01-2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000007997,569965,R,2020-03-31 13:06:30.814448+01,11-01-2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000007702,574222,R,2020-03-31 13:10:21.420187+01,11-01-2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000014174,568703,C,2020-03-31 13:21:37.94545+01,30-12-2016,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV172471J,,,";
+                    "9000007053,571645,R,2020-03-31 12:11:47.339148+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000009808,333330,R,2020-03-31 12:49:47.513821+01,2016-09-05,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
+                    "9000006316,570294,R,2020-03-31 12:52:13.463901+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007997,569965,R,2020-03-31 13:06:30.814448+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007702,574222,R,2020-03-31 13:10:21.420187+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000014174,568703,C,2020-03-31 13:21:37.94545+01,2016-12-30,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV172471J,,,";
 
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
@@ -95,12 +117,12 @@ public class ReceiveDataTests
     {
         // Arrange
         string data = "nhs_number,episode_id,episode_type,change_db_date_time,episode_date,appointment_made,date_of_foa,date_of_as,early_recall_date,call_recall_status_authorised_by,end_code,end_code_last_updated,bso_organisation_code,bso_batch_id,reason_closed_code,end_point,final_action_code\n" +
-                    "9000007053,571645,R,2020/03/31 12:11:47.339148+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000009808,333330,R,2020/03/31 12:49:47.513821+01,05/09/2016,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV000001A,,,\n" +
-                    "9000006316,570294,R,2020/03/31 12:52:13.463901+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000007997,569965,R,2020/03/31 13:06:30.814448+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000007702,574222,R,2020/03/31 13:10:21.420187+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000014174,568703,C,2020/03/31 13:21:37.94545+01,30/12/2016,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV172471J,,,";
+                    "9000007053,571645,R,2020/03/31 12:11:47.339148+01,2017/01/11,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000009808,333330,R,2020/03/31 12:49:47.513821+01,2016/09/05,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV000001A,,,\n" +
+                    "9000006316,570294,R,2020/03/31 12:52:13.463901+01,2017/01/11,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007997,569965,R,2020/03/31 13:06:30.814448+01,2017/01/11,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007702,574222,R,2020/03/31 13:10:21.420187+01,2017/01/11,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000014174,568703,C,2020/03/31 13:21:37.94545+01,2016/12/30,True,,,,SCREENING_OFFICE,SC,2020/03/31 00:00:00+01,LAV,LAV172471J,,,";
 
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
@@ -120,8 +142,8 @@ public class ReceiveDataTests
         // Arrange
         string data = "nhs_number,episode_id,episode_type,change_db_date_time,episode_date,appointment_made,date_of_foa,date_of_as,early_recall_date,call_recall_status_authorised_by,end_code,end_code_last_updated,bso_organisation_code,bso_batch_id,reason_closed_code,end_point,final_action_code\n" +
                     "9000007053,571645,R,2020-03-31 12:11:47.339148+01,11-INVALID-20555,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000009808,333330,R,2020-03-31 12:49:47.513821+01,32/01/2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
-                    "9000006316,570294,R,2020-03-31 12:52:13.463901+01,2017-02-30,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,";
+                    "9000009808,333330,R,2020-03-31 12:49:47.513821+01,2017-01-33,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
+                    "9000006316,570294,R,2020-03-31 12:52:13.463901+01,201-02-30,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,";
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
 
@@ -144,10 +166,10 @@ public class ReceiveDataTests
     {
         // Arrange
         string data = "change_db_date_time,nhs_number,superseded_nhs_number,gp_practice_code,bso_organisation_code,next_test_due_date,subject_status_code,early_recall_date,latest_invitation_date,removal_reason,removal_date,reason_for_ceasing_code,is_higher_risk,higher_risk_next_test_due_date,hr_recall_due_date,higher_risk_referral_reason_code,date_irradiated,is_higher_risk_active,gene_code,ntdd_calculation_method,preferred_language\n" +
-                "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
-                "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,05/05/2019,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
-                "2020-03-31 12:52:13.463901+01,9000006316,,A00017,LAV,11/01/202,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
-                "2020-03-31 13:06:30.814448+01,9000007997,,A00018,LAV,11/01/202,NORMAL,,2017-01-11,,,,False,,,,,,,,";
+                "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
+                "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,2019-05-05,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
+                "2020-03-31 12:52:13.463901+01,9000006316,,A00017,LAV,2022-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
+                "2020-03-31 13:06:30.814448+01,9000007997,,A00018,LAV,2022-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,";
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
 
@@ -164,8 +186,8 @@ public class ReceiveDataTests
     {
         // Arrange
         string data = "nhs_number,episode_id,episode_type,change_db_date_time,episode_date,appointment_made,date_of_foa,date_of_as,early_recall_date,call_recall_status_authorised_by,end_code,end_code_last_updated,bso_organisation_code,bso_batch_id,reason_closed_code,end_point,final_action_code\n" +
-                    "9000007053,571645,R,2020-03-31 12:11:47.339148+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000009808,333330,R,2020-03-31 12:49:47.513821+01,05/09/2016,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
+                    "9000007053,571645,R,2020-03-31 12:11:47.339148+01,2017-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000009808,333330,R,2020-03-31 12:49:47.513821+01,2016-09-05,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
                     "BadRow,,,,\n" +
                     "BadRow,,,,\n";
 
@@ -193,12 +215,12 @@ public class ReceiveDataTests
     {
         // Arrange
         string data = "change_db_date_time,nhs_number,superseded_nhs_number,gp_practice_code,bso_organisation_code,next_test_due_date,subject_status_code,early_recall_date,latest_invitation_date,removal_reason,removal_date,reason_for_ceasing_code,is_higher_risk,higher_risk_next_test_due_date,hr_recall_due_date,higher_risk_referral_reason_code,date_irradiated,is_higher_risk_active,gene_code,ntdd_calculation_method,preferred_language\n" +
-                "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
-                "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,05/05/2019,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
+                "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
+                "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,2019-05-05,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
                 "BadRow,,,,\n" +
                 "BadRow,,,,\n" +
-                "2020-03-31 12:52:13.463901+01,9000006316,,A00017,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
-                "2020-03-31 13:06:30.814448+01,9000007997,,A00018,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,";
+                "2020-03-31 12:52:13.463901+01,9000006316,,A00017,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
+                "2020-03-31 13:06:30.814448+01,9000007997,,A00018,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,";
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
 
@@ -224,10 +246,10 @@ public class ReceiveDataTests
     {
         // Arrange
         string data = "change_db_date_time,nhs_number,superseded_nhs_number,gp_practice_code,bso_organisation_code,next_test_due_date,subject_status_code,early_recall_date,latest_invitation_date,removal_reason,removal_date,reason_for_ceasing_code,is_higher_risk,higher_risk_next_test_due_date,hr_recall_due_date,higher_risk_referral_reason_code,date_irradiated,is_higher_risk_active,gene_code,ntdd_calculation_method,preferred_language\n" +
-                "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
-                "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,05/09/2019,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
-                "2020-03-31 12:52:13.463901+01,9000006316,,A00017,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
-                "2020-03-31 13:06:30.814448+01,9000007997,,A00018,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,";
+                "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
+                "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,2019-09-05,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
+                "2020-03-31 12:52:13.463901+01,9000006316,,A00017,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
+                "2020-03-31 13:06:30.814448+01,9000007997,,A00018,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,";
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
 
@@ -277,10 +299,10 @@ public class ReceiveDataTests
     {
         // Arrange
         string data = "INVALID,nhs_number,superseded_nhs_number,gp_practice_code,bso_organisation_code,next_test_due_date,subject_status_code,early_recall_date,latest_invitation_date,removal_reason,removal_date,reason_for_ceasing_code,is_higher_risk,higher_risk_next_test_due_date,hr_recall_due_date,higher_risk_referral_reason_code,date_irradiated,is_higher_risk_active,gene_code,ntdd_calculation_method,preferred_language\n" +
-                "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
-                "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,05/09/2019,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
-                "2020-03-31 12:52:13.463901+01,9000006316,,A00017,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
-                "2020-03-31 13:06:30.814448+01,9000007997,,A00018,LAV,11/01/2020,NORMAL,,2017-01-11,,,,False,,,,,,,,";
+                "2020-03-31 12:11:47.339148+01,9000007053,,A00014,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
+                "2020-03-31 12:49:47.513821+01,9000009808,,A00009,LAV,2019-09-05,NORMAL,,2016-09-05,,,,False,,,,,,,,\n" +
+                "2020-03-31 12:52:13.463901+01,9000006316,,A00017,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,\n" +
+                "2020-03-31 13:06:30.814448+01,9000007997,,A00018,LAV,2020-01-11,NORMAL,,2017-01-11,,,,False,,,,,,,,";
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
 
@@ -305,10 +327,10 @@ public class ReceiveDataTests
     {
         // Arrange
         string data = "nhs_number,episode_id,episode_type,change_db_date_time,episode_date,appointment_made,date_of_foa,date_of_as,early_recall_date,call_recall_status_authorised_by,end_code,end_code_last_updated,bso_organisation_code,bso_batch_id,reason_closed_code,end_point,final_action_code\n" +
-                    "9000007053,571645,R,2020-03-31 12:11:47.339148+01,11/01/2020,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007053,571645,R,2020-03-31 12:11:47.339148+01,2020-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
                     "9000009808,333330,R,2020-03-31 12:49:47.513821+01,05/09/2016,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV000001A,,,\n" +
-                    "9000006316,570294,R,2020-03-31 12:52:13.463901+01,11/01/2020,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
-                    "9000007997,569965,R,2020-03-31 13:06:30.814448+01,11/01/2020,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000006316,570294,R,2020-03-31 12:52:13.463901+01,2020-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
+                    "9000007997,569965,R,2020-03-31 13:06:30.814448+01,2020-01-11,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
                     "9000007702,574222,R,2020-03-31 13:10:21.420187+01,11/01/2017,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV121798J,,,\n" +
                     "9000014174,568703,C,2020-03-31 13:21:37.94545+01,30/12/2016,True,,,,SCREENING_OFFICE,SC,2020-03-31 00:00:00+01,LAV,LAV172471J,,,";
 
