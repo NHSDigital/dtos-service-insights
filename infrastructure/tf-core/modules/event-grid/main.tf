203b436c442b39f39973dd27ab9b887eecdb719f
Alastair Lock <alastair.lock@gmail.com>
2024-12-06 16:49:58 +0000

    wip

--- infrastructure/tf-core/modules/event-grid/main.tf
+++ infrastructure/tf-core/modules/event-grid/main.tf
@@ -19,10 +19,10 @@ resource "azurerm_eventgrid_topic" "azurerm_eventgrid" {
 
 }
 
-# resource "azurerm_storage_queue" "storage_queue" {
-#   name                 = "${var.subscription_name}-storage-queue"
-#   storage_account_name = var.dead_letter_storage_account_name
-# }
+resource "azurerm_storage_queue" "storage_queue" {
+  name                 = "${var.subscription_name}-storage-queue"
+  storage_account_name = var.dead_letter_storage_account_name
+}
 
 
 resource "azurerm_eventgrid_event_subscription" "eventgrid_event_subscription" {
@@ -33,15 +33,15 @@ resource "azurerm_eventgrid_event_subscription" "eventgrid_event_subscription" {
   #   blob_container_name = dead_letter_storage_account_container_name
   # }
 
-  # storage_queue_endpoint {
-  #   storage_account_id  = var.dead_letter_storage_account_id
-  #   queue_name          = azurerm_storage_queue.storage_queue.name
-  # }
+  storage_queue_endpoint {
+    storage_account_id  = var.dead_letter_storage_account_id
+    queue_name          = azurerm_storage_queue.storage_queue.name
+  }
 
   # HTTP endpoint for the second function app (create participant screening episode)
-  webhook_endpoint {
-    url = var.function_app_endpoint  # URL of the second function app that will process the event
-  }
+  # webhook_endpoint {
+  #   url = var.function_app_endpoint  # URL of the second function app that will process the event
+  # }
 
   storage_blob_dead_letter_destination {
     storage_account_id          = var.dead_letter_storage_account_id
