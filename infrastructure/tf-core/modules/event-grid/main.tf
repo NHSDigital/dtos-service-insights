cc6b8ce59307219d1b1a4f5cdd55a4f7747466b2
Alastair Lock <alastair.lock@gmail.com>
2024-12-09 16:13:20 +0000

    wip

--- infrastructure/tf-core/modules/event-grid/main.tf
+++ infrastructure/tf-core/modules/event-grid/main.tf
@@ -33,15 +33,15 @@ resource "azurerm_eventgrid_event_subscription" "eventgrid_event_subscription" {
   #   blob_container_name = dead_letter_storage_account_container_name
   # }
 
-  storage_queue_endpoint {
-    storage_account_id  = var.dead_letter_storage_account_id
-    queue_name          = azurerm_storage_queue.storage_queue.name
-  }
+  # storage_queue_endpoint {
+  #   storage_account_id  = var.dead_letter_storage_account_id
+  #   queue_name          = azurerm_storage_queue.storage_queue.name
+  # }
 
   # HTTP endpoint for the second function app (create participant screening episode)
-  # webhook_endpoint {
-  #   url = var.function_app_endpoint  # URL of the second function app that will process the event
-  # }
+  webhook_endpoint {
+    url = var.function_app_endpoint  # URL of the second function app that will process the event
+  }
 
   storage_blob_dead_letter_destination {
     storage_account_id          = var.dead_letter_storage_account_id
