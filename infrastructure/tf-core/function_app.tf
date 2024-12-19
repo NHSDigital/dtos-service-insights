3eae9b112d18b1de27d13ea1371939416183de26
Alastair Lock <alastair.lock@gmail.com>
2024-12-19 15:38:28 +0000

    wip

--- infrastructure/tf-core/function_app.tf
+++ infrastructure/tf-core/function_app.tf
@@ -20,10 +20,10 @@ module "functionapp" {
   # rbac_role_assignments = local.rbac_role_assignments[each.value.region]
   rbac_role_assignments = each.value.rbac_role_assignments
 
-  asp_id = module.app-service-plan["${each.value.function_config.app_service_plan_key}-${each.value.region}"].app_service_plan_id
+  asp_id = module.app-service-plan["${each.value.app_service_plan_key}-${each.value.region}"].app_service_plan_id
 
   # Use the storage account assigned identity for the Function Apps:
-  storage_account_name          = module.storage["fnapp-${each.value.region_key}"].storage_account_name
+  storage_account_name          = module.storage["fnapp-${each.value.region}"].storage_account_name
   storage_account_access_key    = var.function_apps.storage_uses_managed_identity == true ? null : module.storage["fnapp-${each.value.region}"].storage_account_primary_access_key
   storage_uses_managed_identity = var.function_apps.storage_uses_managed_identity
 
@@ -48,7 +48,7 @@ module "functionapp" {
 
   # Private Endpoint Configuration if enabled
   private_endpoint_properties = var.features.private_endpoints_enabled ? {
-    private_dns_zone_ids                 = [data.terraform_remote_state.hub.outputs.private_dns_zones["${each.value.region_key}-app_services"].id]
+    private_dns_zone_ids                 = [data.terraform_remote_state.hub.outputs.private_dns_zones["${each.value.region}-app_services"].id]
     private_endpoint_enabled             = var.features.private_endpoints_enabled
     private_endpoint_subnet_id           = module.subnets["${module.regions_config[each.value.region].names.subnet}-pep"].id
     private_endpoint_resource_group_name = azurerm_resource_group.rg_private_endpoints[each.value.region].name
