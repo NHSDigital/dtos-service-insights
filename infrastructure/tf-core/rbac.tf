29cc20e283d692fc6f21757cb8c69c8778ca3dad
Alastair Lock <alastair.lock@gmail.com>
2024-12-20 17:13:55 +0000

    pr changes

--- infrastructure/tf-core/rbac.tf
+++ infrastructure/tf-core/rbac.tf
@@ -1,11 +1,5 @@
 locals {
 
-  wip_rbac_roles_storage = [ # this is needed to use the updated storage module - will refactor the other vars here later when function_app.tf is revised
-    "Storage Account Contributor",
-    "Storage Blob Data Owner",
-    "Storage Queue Data Contributor"
-  ]
-
   rbac_roles_key_vault_officers = [
     "Key Vault Certificate Officer",
     "Key Vault Crypto Officer",
