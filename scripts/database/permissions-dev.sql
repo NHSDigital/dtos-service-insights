8c1c9d496e05756fe010fc40cb2d3788e8a1d7be
Alastair Lock <alastair.lock@gmail.com>
2024-12-20 15:00:53 +0000

    feat: dtoss 6235 create terraform module for azure event grid (#102)
    * initial setup of the event grid
    
    * rename create-participant-screening-episode due to length limit
    
    * Updated database permission scripts
    
    * adding in file
    
    * pr changes

--- scripts/database/permissions-dev.sql
+++ scripts/database/permissions-dev.sql
@@ -6,6 +6,22 @@ CREATE USER [dev-uks-si-create-ps-profile-data] FROM EXTERNAL PROVIDER;
 ALTER ROLE [db_datareader] ADD MEMBER [dev-uks-si-create-ps-profile-data];
 ALTER ROLE [db_datawriter] ADD MEMBER [dev-uks-si-create-ps-profile-data];
 
+CREATE USER [dev-uks-si-get-ps-profile-data] FROM EXTERNAL PROVIDER;
+ALTER ROLE [db_datareader] ADD MEMBER [dev-uks-si-get-ps-profile-data];
+ALTER ROLE [db_datawriter] ADD MEMBER [dev-uks-si-get-ps-profile-data];
+
+CREATE USER [dev-uks-si-get-ps-profile] FROM EXTERNAL PROVIDER;
+ALTER ROLE [db_datareader] ADD MEMBER [dev-uks-si-get-ps-profile];
+ALTER ROLE [db_datawriter] ADD MEMBER [dev-uks-si-get-ps-profile];
+
+CREATE USER [dev-uks-si-get-ps-episode-data] FROM EXTERNAL PROVIDER;
+ALTER ROLE [db_datareader] ADD MEMBER [dev-uks-si-get-ps-episode-data];
+ALTER ROLE [db_datawriter] ADD MEMBER [dev-uks-si-get-ps-episode-data];
+
+CREATE USER [dev-uks-si-get-ps-episode] FROM EXTERNAL PROVIDER;
+ALTER ROLE [db_datareader] ADD MEMBER [dev-uks-si-get-ps-episode];
+ALTER ROLE [db_datawriter] ADD MEMBER [dev-uks-si-get-ps-episode];
+
 CREATE USER [dev-uks-si-create-ps-episode] FROM EXTERNAL PROVIDER;
 ALTER ROLE [db_datareader] ADD MEMBER [dev-uks-si-create-ps-episode];
 ALTER ROLE [db_datawriter] ADD MEMBER [dev-uks-si-create-ps-episode];
