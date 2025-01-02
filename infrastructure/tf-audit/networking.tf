21089b7907356c67137955975f2bbde02de451b2
Alastair Lock <alastair.lock@gmail.com>
2025-01-02 14:09:55 +0000

    Remove the dns from the vnet module

--- infrastructure/tf-audit/networking.tf
+++ infrastructure/tf-audit/networking.tf
@@ -30,8 +30,6 @@ module "vnet" {
   monitor_diagnostic_setting_vnet_enabled_logs = local.monitor_diagnostic_setting_vnet_enabled_logs
   monitor_diagnostic_setting_vnet_metrics      = local.monitor_diagnostic_setting_vnet_metrics
 
-  dns_servers = [data.terraform_remote_state.hub.outputs.private_dns_resolver_inbound_ips[each.key].private_dns_resolver_ip]
-
   tags = var.tags
 }
 
