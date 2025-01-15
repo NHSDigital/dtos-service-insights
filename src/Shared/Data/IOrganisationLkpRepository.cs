c0c7e6a6f225fd317e6dfb2d0e455d05e4b55d98
OswellHita <oswell.hita1@nhs.net>
2025-01-15 22:24:54 +0000

    feat: added GetOrganisationIdAsync method to OrganisationLkpRepository to retrieve OrganisationId by OrganisationCode

--- src/Shared/Data/IOrganisationLkpRepository.cs
+++ src/Shared/Data/IOrganisationLkpRepository.cs
@@ -4,5 +4,8 @@ namespace NHS.ServiceInsights.Data;
 
 public interface IOrganisationLkpRepository
 {
+    Task<long?> GetOrganisationIdAsync(string organisationCode);
+    Task<OrganisationLkp?> GetOrganisationLkp(string organisationCode);
     Task<OrganisationLkp?> GetOrganisationAsync(long organisationId);
+    Task<IEnumerable<OrganisationLkp>> GetAllOrganisationsAsync();
 }
