6f54f79e07d22262397ce03a56314e4edf318fa7
OswellHita <oswell.hita1@nhs.net>
2025-01-15 21:30:54 +0000

    refactor: Add Event Grid to ReceiveData function

--- src/EpisodeIntegrationService/ReceiveData/ReceiveData.cs
+++ src/EpisodeIntegrationService/ReceiveData/ReceiveData.cs
@@ -5,6 +5,7 @@ using Microsoft.Azure.Functions.Worker;
 using System.Text.Json;
 using CsvHelper;
 using System.Globalization;
+using Azure.Messaging.EventGrid;
 
 namespace NHS.ServiceInsights.EpisodeIntegrationService;
 
@@ -12,6 +13,7 @@ public class ReceiveData
 {
     private readonly ILogger<ReceiveData> _logger;
     private readonly IHttpRequestService _httpRequestService;
+    private readonly EventGridPublisherClient _eventGridPublisherClient;
     private readonly string[] episodesExpectedHeaders = new[] { "nhs_number", "episode_id", "episode_type", "change_db_date_time", "episode_date", "appointment_made", "date_of_foa", "date_of_as", "early_recall_date", "call_recall_status_authorised_by", "end_code", "end_code_last_updated", "bso_organisation_code", "bso_batch_id", "reason_closed_code", "end_point", "final_action_code" };
     private readonly string[] subjectsExpectedHeaders = new[] { "change_db_date_time", "nhs_number", "superseded_nhs_number", "gp_practice_code", "bso_organisation_code", "next_test_due_date", "subject_status_code", "early_recall_date", "latest_invitation_date", "removal_reason", "removal_date", "reason_for_ceasing_code", "is_higher_risk", "higher_risk_next_test_due_date", "hr_recall_due_date", "higher_risk_referral_reason_code", "date_irradiated", "is_higher_risk_active", "gene_code", "ntdd_calculation_method", "preferred_language" };
 
@@ -23,10 +25,11 @@ public class ReceiveData
     private int episodeFailureCount = 0;
     private int episodeRowIndex = 0;
 
-    public ReceiveData(ILogger<ReceiveData> logger, IHttpRequestService httpRequestService)
+    public ReceiveData(ILogger<ReceiveData> logger, IHttpRequestService httpRequestService, EventGridPublisherClient eventGridPublisherClient)
     {
         _logger = logger;
         _httpRequestService = httpRequestService;
+        _eventGridPublisherClient = eventGridPublisherClient;
 
     }
 
@@ -67,9 +70,8 @@ public class ReceiveData
                     var episodesEnumerator = csv.GetRecords<BssEpisode>();
                     if (name.EndsWith("_historic.csv"))
                     {
-                        string? url = Environment.GetEnvironmentVariable("EventGridUrl");
                         Dictionary<string, string> referenceData = await RetrieveReferenceDataAsync();
-                        await ProcessHistoricalEpisodeDataAsync(name, episodesEnumerator, url, referenceData);
+                        await ProcessHistoricalEpisodeDataAsync(name, episodesEnumerator, referenceData);
                     }
                     else
                     {
@@ -193,7 +195,7 @@ public class ReceiveData
     }
 
 
-    private async Task ProcessHistoricalEpisodeDataAsync(string name, IEnumerable<BssEpisode> episodes, string url, Dictionary<string, string> referenceData)
+    private async Task ProcessHistoricalEpisodeDataAsync(string name, IEnumerable<BssEpisode> episodes, Dictionary<string, string> referenceData)
     {
         try
         {
@@ -202,12 +204,14 @@ public class ReceiveData
             foreach (var episode in episodes)
             {
                 var modifiedEpisode = MapHistoricalEpisodeToEpisodeDto(episode, referenceData);
-                string serializedEpisode = JsonSerializer.Serialize(modifiedEpisode);
-
-                _logger.LogInformation("Sending Episode to {Url}: {Request}", url, serializedEpisode);
-
-                await _httpRequestService.SendPost(url, serializedEpisode);
-
+                EventGridEvent eventGridEvent = new EventGridEvent(
+                    subject: "Episode Created",
+                    eventType: "CreateParticipantScreeningEpisode",
+                    dataVersion: "1.0",
+                    data: modifiedEpisode
+                );
+                _logger.LogInformation("Sending event to Event Grid: {EventGridEvent}", JsonSerializer.Serialize(eventGridEvent));
+                _eventGridPublisherClient.SendEvent(eventGridEvent);
                 episodeSuccessCount++;
                 episodeRowIndex++;
                 _logger.LogInformation("Row No.{rowIndex} processed successfully",episodeRowIndex);
@@ -220,7 +224,7 @@ public class ReceiveData
             episodeFailureCount++;
             episodeRowIndex++;
             _logger.LogInformation("Row No.{rowIndex} processed unsuccessfully",episodeRowIndex);
-            await ProcessHistoricalEpisodeDataAsync(name,episodes, url, referenceData);
+            await ProcessHistoricalEpisodeDataAsync(name,episodes, referenceData);
         }
     }
 
@@ -448,5 +452,6 @@ public class ReceiveData
             return (parts[0], parts[1]);
         }
     }
+
 }
 
