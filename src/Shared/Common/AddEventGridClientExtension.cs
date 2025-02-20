74e92e2306619bdb80ba9b36f16378dee92e1c06
OswellHita <oswell.hita1@nhs.net>
2025-02-20 16:16:15 +0000

    refactor: update code to reduce complexity

--- src/Shared/Common/AddEventGridClientExtension.cs
+++ src/Shared/Common/AddEventGridClientExtension.cs
@@ -1,6 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Azure.Messaging.EventGrid;
 using Azure.Identity;
 
 namespace NHS.ServiceInsights.Common;
