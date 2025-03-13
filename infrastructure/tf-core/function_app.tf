345e3469149c38ac4f919ee7b7c10d1896d5db08
Alastair Lock <alastair.lock@gmail.com>
2025-03-13 16:33:24 +0000

    wip

--- infrastructure/tf-core/function_app.tf
+++ infrastructure/tf-core/function_app.tf
@@ -112,7 +112,6 @@ resource "azurerm_role_assignment" "data_sender" {
   Local variables used to create the Environment Variables for the Function Apps
 -------------------------------------------------------------------------------------------------- */
 locals {
-  primary_region = [for k, v in var.regions : k if v.is_primary_region][0]
 
   app_settings_common = {
     DOCKER_ENABLE_CI                    = var.function_apps.docker_CI_enable
