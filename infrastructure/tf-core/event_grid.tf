aefd555af4e81b521ea9fce05d4247a9f0a7d428
Alastair Lock <alastair.lock@gmail.com>
2025-03-24 16:05:58 +0000

    wip

--- infrastructure/tf-core/event_grid.tf
+++ infrastructure/tf-core/event_grid.tf
@@ -6,37 +6,43 @@ module "event_grid_topic" {
   topic_name          = each.value.event_grid_subscription_key
   resource_group_name = azurerm_resource_group.core[each.value.region].name
   location            = each.value.region
-  # identity_type       = each.value.identity_type
-  identity_type    = "SystemAssigned"
-  inbound_ip_rules = each.value.inbound_ip_rules
+  # identity_type     = each.value.identity_type
+  identity_type       = "SystemAssigned"
+  inbound_ip_rules    = each.value.inbound_ip_rules
 
   # Private Endpoint Configuration if enabled
-  private_endpoint_properties = null
+  private_endpoint_properties = var.features.private_endpoints_enabled ? {
+     private_dns_zone_ids                 = [data.terraform_remote_state.hub.outputs.private_dns_zones["${each.value.region}-event_grid"].id]
+     private_endpoint_enabled             = var.features.private_endpoints_enabled
+     private_endpoint_subnet_id           = module.subnets["${module.regions_config[each.value.region].names.subnet}-pep"].id
+     private_endpoint_resource_group_name = azurerm_resource_group.rg_private_endpoints[each.value.region].name
+     private_service_connection_is_manual = var.features.private_service_connection_is_manual
+   } : null
 
   tags = var.tags
 }
 
-# module "event_grid_subscription" {
-#   for_each = local.event_grid_map
+module "event_grid_subscription" {
+  for_each = local.event_grid_map
 
-#   source = "../../../dtos-devops-templates/infrastructure/modules/event-grid-subscription"
+  source = "../../../dtos-devops-templates/infrastructure/modules/event-grid-subscription"
 
-#   subscription_name   = each.value.event_grid_subscription_key
-#   resource_group_name = azurerm_resource_group.core[each.value.region].name
-#   # azurerm_eventgrid_id = data.terraform_remote_state.hub.outputs.event_grid_topic["${each.value.event_grid_subscription_key}-${each.value.region}"].id
-#   azurerm_eventgrid_id = var.features.event_grid_topic_enabled_in_project_vnet == true ? module.event_grid_topic["${each.value.event_grid_subscription_key}-${each.value.region}"].id : data.terraform_remote_state.hub.outputs.event_grid_topic["${each.value.event_grid_subscription_key}-${each.value.region}"].id
+  subscription_name   = each.value.event_grid_subscription_key
+  resource_group_name = azurerm_resource_group.core[each.value.region].name
+  # azurerm_eventgrid_id = data.terraform_remote_state.hub.outputs.event_grid_topic["${each.value.event_grid_subscription_key}-${each.value.region}"].id
+  azurerm_eventgrid_id = var.features.event_grid_topic_enabled_in_project_vnet == true ? module.event_grid_topic["${each.value.event_grid_subscription_key}-${each.value.region}"].id : data.terraform_remote_state.hub.outputs.event_grid_topic["${each.value.event_grid_subscription_key}-${each.value.region}"].id
 
-#   function_endpoint = format("%s/functions/%s", module.functionapp["${each.value.subscriber_functionName}-${each.value.region}"].id, each.value.subscriber_functionName)
-#   principal_id      = module.functionapp["${each.value.subscriber_functionName}-${each.value.region}"].function_app_sami_id
+  function_endpoint = format("%s/functions/%s", module.functionapp["${each.value.subscriber_functionName}-${each.value.region}"].id, each.value.subscriber_functionName)
+  principal_id      = module.functionapp["${each.value.subscriber_functionName}-${each.value.region}"].function_app_sami_id
 
-#   dead_letter_storage_account_container_name = "deadletterqueue"
-#   dead_letter_storage_account_id             = module.storage["eventgrid-${each.value.region}"].storage_account_id
+  dead_letter_storage_account_container_name = "deadletterqueue"
+  dead_letter_storage_account_id             = module.storage["eventgrid-${each.value.region}"].storage_account_id
 
-#   tags = var.tags
+  tags = var.tags
 
-#   depends_on = [module.functionapp]
+  depends_on = [module.functionapp]
 
-# }
+}
 
 locals {
 
