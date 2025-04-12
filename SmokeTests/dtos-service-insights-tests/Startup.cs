4b1a5931e3cf710ccc44fa0952caf98c61c4fe1b
NageswarU <nageswar.undapalli1@nhs.net>
2025-04-12 21:13:28 +0100

    DTOSS-8065: add api helper validation methods

--- SmokeTests/dtos-service-insights-tests/Startup.cs
+++ SmokeTests/dtos-service-insights-tests/Startup.cs
@@ -47,6 +47,7 @@ internal static class Startup
             return new Azure.Storage.Blobs.BlobServiceClient(connectionString);
         });
         services.AddSingleton<BlobStorageHelper>();
+        services.AddSingleton<ApiClientHelper>();
         services.AddSingleton(sp => sp.GetRequiredService<IOptions<AppSettings>>().Value);
         services.AddTransient<EndToEndFileUploadService>();
 
