ce89d5837d9eb2a49408d154f70a46678d2f6179
Alastair Lock <alastair.lock@gmail.com>
2025-04-15 15:25:36 +0100

    DTOSS-8458: Create a Terraform module for Azure Action Group

--- infrastructure/tf-core/monitor.tf
+++ infrastructure/tf-core/monitor.tf
@@ -1,20 +1,14 @@
 module "monitor_action_group" {
-  for_each = local.monitor_action_group_map
+  for_each = var.regions
 
   source = "./modules/monitor-action-group"
 
-  name                = lower("example-monitor-action-group-${each.value.region}")
-  resource_group_name = azurerm_resource_group.core[each.value.region].name
-  location            = each.value.region
-  short_name          = each.value.short_name
+  name                = lower("example-monitor-action-group-${each.key}")
+  resource_group_name = azurerm_resource_group.core[each.key].name
+  location            = each.key
+  short_name          = var.monitor_action_group.short_name
 
-  email_receiver = [
-    {
-      name                    = each.value.name
-      email_address           = each.value.email_address
-      use_common_alert_schema = false
-    }
-  ]
+  email_receiver = local.email_receiver_list
 
   # webhook_receiver = [
   #   {
@@ -27,23 +21,20 @@ module "monitor_action_group" {
 
 locals {
 
-  monitor_action_group_list = flatten([
-    for region in keys(var.regions) : [
-      for email_receiver_key, email_receiver_details in var.monitor_action_group.email_receiver : merge(
-        {
-          region             = region             # 1st iterator
-          email_receiver_key = email_receiver_key # 2nd iterator
-          short_name         = var.monitor_action_group.short_name
-        },
-        email_receiver_details # the rest of the key/value pairs for a specific event_grids
-      )
-    ]
+  email_receiver_list = flatten([
+    for email_receiver_key, email_receiver_details in var.monitor_action_group.email_receiver : merge(
+      # {
+      #   region             = region             # 1st iterator
+      #   email_receiver_key = email_receiver_key # 2nd iterator
+      # },
+      email_receiver_details # the rest of the key/value pairs for a specific event_grids
+    )
   ])
 
   # ...then project the list of objects into a map with unique keys (combining the iterators), for consumption by a for_each meta argument
-  monitor_action_group_map = {
-    for object in local.monitor_action_group_list : "${object.email_receiver_key}-${object.region}" => object
-  }
+  # email_receiver_map = {
+  #   for object in local.email_receiver_list : "${object.email_receiver_key}-${object.region}" => object
+  # }
 
 
 }
