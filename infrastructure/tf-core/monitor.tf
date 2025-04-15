e65dd6183934d5f9304fed36404429b60f84f823
Alastair Lock <alastair.lock@gmail.com>
2025-04-15 16:41:25 +0100

    DTOSS-8458: Create a Terraform module for Azure Action Group

--- infrastructure/tf-core/monitor.tf
+++ infrastructure/tf-core/monitor.tf
@@ -1,9 +1,9 @@
 module "monitor_action_group" {
   for_each = var.regions
 
-  source = "./modules/monitor-action-group"
+  source = "../../../dtos-devops-templates/infrastructure/modules/monitor-action-group"
 
-  name                = lower("example-monitor-action-group-${each.key}")
+  name                = module.regions_config[each.key].names.monitor-action-group
   resource_group_name = azurerm_resource_group.core[each.key].name
   location            = each.key
   short_name          = var.monitor_action_group.short_name
@@ -15,5 +15,4 @@ module "monitor_action_group" {
 locals {
   email_receiver_list = values(var.monitor_action_group.email_receiver)
   webhook_receiver_list = values(var.monitor_action_group.webhook_receiver)
-
 }
